4e0bcf351c677d18bf7f1e10f9d34b3f0b0d3a96
Tijo <emailtijothomas@gmail.com>
2017-01-20 16:17:53 -0600

    Adds mixin functionality to handle media queries.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,13 +3,14 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 // return is used so gulp is aware when the function completes.
 // postcss() is actually expecting an array
 gulp.task('styles', function() {
   return gulp.src('app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
     .on('error', function(errorInfo) {
       console.log(errorInfo.toString());
       this.emit('end');
