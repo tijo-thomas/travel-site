1e16c1bacf1e7bce5e8b388f70db98fa8510b1ca
Tijo <emailtijothomas@gmail.com>
2017-01-28 14:29:38 -0600

    Adds copySpriteGraphic gulp task.

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -5,7 +5,7 @@ rename = require('gulp-rename');
 var config = {
   mode: {
     css: {
-      sprite: 'svg/sprite.svg'
+      sprite: 'sprite.svg',
       render: {
         css: {
           template: './gulp/templates/sprite.css'
@@ -21,10 +21,16 @@ gulp.task('createSprite', function() {
     .pipe(gulp.dest('./app/temp/sprite/'));
 });
 
+// Moves sprite.svg file into projects main images folder.
+gulp.task('copySpriteGraphic', ['createSprite'], function() {
+  return gulp.src('./app/temp/sprite/css/**/*.svg')
+    .pipe(gulp.dest('./app/assets/images/sprites'));
+});
+
 gulp.task('copySpriteCSS', ['createSprite'], function() {
   return gulp.src('./app/temp/sprite/css/*.css')
     .pipe(rename('_sprite.css'))
     .pipe(gulp.dest('./app/assets/styles/modules'));
 });
 
-gulp.task('icons', ['createSprite', 'copySpriteCSS']);
+gulp.task('icons', ['createSprite', 'copySpriteGraphic', 'copySpriteCSS']);
