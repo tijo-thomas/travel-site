a9d6f8f0a45cb07af71f8b9492e34edb2639dbf1
Tijo <emailtijothomas@gmail.com>
2017-02-03 18:32:13 -0600

    Implements reveal on scroll functionality to 'Our Features' section.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,5 @@
 import MobileMenu from './modules/MobileMenu.js';
+import RevealOnScroll from './modules/RevealOnScroll.js';
 
 var mobileMenu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll();
